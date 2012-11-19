f522eb68c9d02491437362e72f98761a5deaa04f
Alexander Leschinsky <lazybadger@ya.ru>
2012-11-20 03:51:53 +0600

    Localization of page

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
   <article class="noposts">
-  	<h2>404 - Content Not Found</h2>
-  	<p>We don't seem to be able to find the content you have requested - why not try a search below?</p>
+  	<h2><?php _e('404 - Content Not Found','fiver' ); ?></h2>
+  	<p><?php _e('We don&rsquo;t seem to be able to find the content you have requested - why not try a search below?','fiver' ); ?></p>
   	<?php get_search_form(); ?>
   </article>
 
